5e8a47fbda7710347951832663da204d63d4b539
Lorenz Keefer <clemenzkafer@gmail.com>
2023-01-19 12:35:02 +0100

    uploads are now taggable and improved app architecture

--- android_app/app/src/main/java/com/example/wallpaperwizard/Fragments/HomeFragment.kt
+++ android_app/app/src/main/java/com/example/wallpaperwizard/Fragments/HomeFragment.kt
@@ -22,6 +22,7 @@ import androidx.fragment.app.Fragment
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import androidx.work.*
 import com.example.wallpaperwizard.*
+import com.example.wallpaperwizard.Components.TagGroup.TagGroup
 import com.example.wallpaperwizard.R
 import com.example.wallpaperwizard.Worker.WallpaperChangerWorker
 import com.google.android.material.chip.Chip
@@ -37,6 +38,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import java.text.SimpleDateFormat
 import java.util.*
+import java.util.stream.Collectors
 
 
 class HomeFragment : Fragment() {
@@ -47,8 +49,8 @@ class HomeFragment : Fragment() {
     lateinit var date_viewer: TextView
     lateinit var notificationManager: NotificationManager
     lateinit var notiProvider: NotificationProvider
-    var selected_tags: MutableList<Chip> = mutableListOf()
-    var unselected_tags: MutableList<Chip> = mutableListOf()
+    val wallpaperApi = RetrofitHelper.getInstance().create(WallpaperApi::class.java)
+    lateinit var tagsResultCallback: Callback<TagsResult>
 
 
     object RetrofitHelper {
@@ -103,72 +105,55 @@ class HomeFragment : Fragment() {
         val tagsPrefsKey = "tags_preferences"
         val syncPrefsKey = "sync_preferences"
 
-        val preferred_tags = prefs.getString(tagsPrefsKey, "")!!.split(";")
+        val preferred_tags = prefs.getString(tagsPrefsKey, "")!!.split(";").stream().filter { str -> str != ""}.collect(Collectors.toList())
         val preferred_sync = prefs.getString(syncPrefsKey, "")
 
         val syncInput: EditText = parent.findViewById(R.id.sync_input)
         syncInput.setText(preferred_sync)
-        val tagGroup: ChipGroup = parent.findViewById(R.id.tag_group)
+        val tagGroup: TagGroup = parent.findViewById(R.id.tag_group)
         time_Viewer = parent.findViewById(R.id.time_viewer)
         date_viewer = parent.findViewById(R.id.date_viewer)
         date_viewer.text = _sdfDateTime.format(Date())
         time_Viewer.text = _sdfWatchTime.format(Date())
 
-        val fetchPopulateTagsCallback: OnTagsResultCallback = object : OnTagsResultCallback {
-            override fun onTagsResult(
-                selectedTags: List<Chip>, unselectedTags: List<Chip>, networkErrorCode: Int
-            ) {
-                if (networkErrorCode != 200) {
-                    Snackbar.make(
-                        main_parent_view,
-                        "Error while getting the Tags. Offline functionality will be implemented soon",
-                        Snackbar.LENGTH_LONG
-                    ).setAction("Retry") { view ->
-                        fetchTags(preferred_tags, this)
-                    }.show()
-                } else {
-                    selected_tags = selectedTags.toMutableList()
-                    unselected_tags = unselectedTags.toMutableList()
-                    populateTags(selectedTags, unselectedTags, tagGroup)
-                }
+        tagsResultCallback = object : Callback<TagsResult> {
+            fun errorHandle(){
+                Snackbar.make(
+                    main_parent_view,
+                    "Error while getting the Tags. Offline functionality will be implemented soon",
+                    Snackbar.LENGTH_LONG
+                ).setAction("Retry") { view ->
+                    wallpaperApi.getTags().enqueue(this)
+                }.show()
+            }
+
+            override fun onResponse(call: Call<TagsResult>, response: Response<TagsResult>) {
                 swipeRefreshView.isRefreshing = false
+                if (response.code() == 200) {
+                    Log.d("tags_response", response.body()!!.tags.toList().toString())
+                    tagGroup.setTags(response.body()!!.tags, preferred_tags.toTypedArray())
+                } else {
+                    errorHandle()
+                }
+            }
+
+            override fun onFailure(call: Call<TagsResult>, t: Throwable) {
+                swipeRefreshView.isRefreshing = false
+                errorHandle()
             }
         }
+
+        wallpaperApi.getTags().enqueue(tagsResultCallback)
+
         swipeRefreshView.setOnRefreshListener { ->
-            fetchTags(preferred_tags, fetchPopulateTagsCallback)
+            wallpaperApi.getTags().enqueue(tagsResultCallback)
         }
 
-        fetchTags(preferred_tags, fetchPopulateTagsCallback)
 
-        tagGroup.setOnCheckedStateChangeListener(object : OnCheckedStateChangeListener {
-            override fun onCheckedChanged(group: ChipGroup, checkedIds: MutableList<Int>) {
-
-                var new_selected_tags: MutableList<Chip> = mutableListOf()
-                var new_unselected_tags: MutableList<Chip> = mutableListOf()
-                for (chip in selected_tags) {
-                    if(chip.id in checkedIds){
-                        new_selected_tags.add(chip)
-                    } else {
-                        new_unselected_tags.add(chip)
-                    }
-                }
-                for (chip in unselected_tags) {
-                    if(chip.id in checkedIds){
-                        new_selected_tags.add(chip)
-                    } else {
-                        new_unselected_tags.add(chip)
-                    }
-                }
-                populateTags(new_selected_tags, new_unselected_tags, tagGroup)
-                selected_tags = new_selected_tags
-                unselected_tags = new_unselected_tags
-            }
-
-        })
 
         applyConfig.setOnClickListener { view ->
             var saved_tags = ""
-            for (id in tagGroup.checkedChipIds) {
+            for (id in tagGroup.tagGroup.checkedChipIds) {
                 val chip = parent.findViewById<Chip>(id)
                 saved_tags += chip.text
                 saved_tags += ";"
@@ -181,7 +166,7 @@ class HomeFragment : Fragment() {
             val syncString = syncInput.text.toString()
             val downloadWorkRequest: WorkRequest =
                 OneTimeWorkRequestBuilder<WallpaperChangerWorker>().setInputData(
-                    Data.Builder().putStringArray("tags", preferred_tags.toTypedArray())
+                    Data.Builder().putStringArray("download_tags", tagGroup.getSelectedTags())
                         .putString("sync", syncString).build()
                 ).build()
             WorkManager.getInstance(context).enqueue(downloadWorkRequest)
@@ -190,57 +175,6 @@ class HomeFragment : Fragment() {
         }
     }
 
-    private fun fetchTags(
-        preferred_tags: List<String>,
-        resultCallback: OnTagsResultCallback,
-    ) {
-        val wallpaperApi = RetrofitHelper.getInstance().create(WallpaperApi::class.java)
-        wallpaperApi.getTags().enqueue(object : Callback<TagsResult> {
-            override fun onResponse(call: Call<TagsResult>, response: Response<TagsResult>) {
-                Log.d("tags_response", response.toString())
-                if (response.code() != 200) {
-                    resultCallback.onTagsResult(listOf(), listOf(), response.code())
-                    return
-                }
-                val tags = response.body()!!.tags
-                val selected_tags: MutableList<Chip> = mutableListOf()
-                val unselected_tags: MutableList<Chip> = mutableListOf()
-                for (tag in tags) {
-                    val view = Chip(
-                        requireContext(),
-                        null,
-                        com.google.android.material.R.style.Widget_MaterialComponents_Chip_Filter
-                    )
-                    view.text = tag
-                    view.id = ViewCompat.generateViewId()
-                    view.isCheckable = true
-                    view.isClickable = true
-
-                    if (tag in preferred_tags) {
-                        view.isChecked = true
-                        selected_tags.add(view)
-                    } else {
-                        unselected_tags.add(view)
-                    }
-                }
-                resultCallback.onTagsResult(selected_tags, unselected_tags, 200)
-            }
-
-            override fun onFailure(call: Call<TagsResult>, t: Throwable) {
-                resultCallback.onTagsResult(listOf(), listOf(), 500)
-            }
-        })
-    }
-
-    fun populateTags(selectedTags: List<Chip>, unselectedTags: List<Chip>, tagGroup: ChipGroup) {
-        tagGroup.removeAllViews()
-        for (chip in selectedTags) {
-            tagGroup.addView(chip)
-        }
-        for (chip in unselectedTags) {
-            tagGroup.addView(chip)
-        }
-    }
 
     override fun onStart() {
         super.onStart()
@@ -259,13 +193,6 @@ class HomeFragment : Fragment() {
     override fun onStop() {
         super.onStop()
         requireContext().unregisterReceiver(_broadcastReceiver)
-    }
-
-
-    interface OnTagsResultCallback {
-        fun onTagsResult(
-            selectedTags: List<Chip>, unselectedTags: List<Chip>, networkErrorCode: Int
-        )
     }
 }
 
--- unix/README.md
+++ unix/README.md
@@ -10,3 +10,7 @@ This will get you a nice sailing boat as a background without syncing to anyone
 ## Install
 There are many ways to install this. One is `# ln -s "$(pwd)/get_wallpaper.sh" /usr/bin/gw`\
 The script will be systemwide accessible with the shortcut `gw`
+
+## Upload
+You can use the post_wallpaper script. As arguments, give url, a directory and some tags (separeted by ";") in that order. 
+The script will then upload all JPG files to the server
