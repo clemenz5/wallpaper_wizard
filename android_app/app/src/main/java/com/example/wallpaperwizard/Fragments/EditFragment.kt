dced97d00b6a794831f8e87a65cb697fb590cbc7
Lorenz Keefer <clemenzkafer@gmail.com>
2023-06-25 15:32:38 +0200

    added tag layout to filter

--- android_app/app/src/main/java/com/example/wallpaperwizard/Fragments/EditFragment.kt
+++ android_app/app/src/main/java/com/example/wallpaperwizard/Fragments/EditFragment.kt
@@ -1,32 +1,48 @@
 package com.example.wallpaperwizard.Fragments
 
 import android.graphics.BitmapFactory
+import android.graphics.Color
 import android.os.Bundle
+import android.util.DisplayMetrics
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
 import android.widget.ImageView
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.wallpaperwizard.Components.TagGroup.OnTagSelectionChangeListener
+import com.example.wallpaperwizard.Components.TagGroup.TagGroup
 import com.example.wallpaperwizard.R
+import com.example.wallpaperwizard.TagsResult
 import com.example.wallpaperwizard.WallpaperApi
 import com.example.wallpaperwizard.WallpaperInfoObject
 import com.example.wallpaperwizard.WallpaperListResponse
+import com.google.android.material.chip.Chip
+import com.google.android.material.snackbar.Snackbar
 import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.security.AccessController.getContext
+import java.util.stream.Collectors
+
 
 val wallpaperApi = EditFragment.RetrofitHelper.getInstance().create(WallpaperApi::class.java)
 
 class EditFragment : Fragment() {
-    var wallpaperList: Array<WallpaperInfoObject> = emptyArray()
+    var wallpaperList: List<WallpaperInfoObject> = emptyList()
+    var shownWallpaper: MutableList<WallpaperInfoObject> = mutableListOf()
+    var selectedWallpaper: MutableList<Int> = mutableListOf()
     lateinit var recyclerView: RecyclerView
     lateinit var recyclerAdapter: CustomAdapter
+    lateinit var tagsResultCallback: Callback<TagsResult>
+    var height: Int = 1920
+    var width: Int = 1080
 
     object RetrofitHelper {
         val baseUrl = "https://ww.keefer.de"
@@ -39,6 +55,11 @@ class EditFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        val displayMetrics = DisplayMetrics()
+        activity?.getWindowManager()?.getDefaultDisplay()?.getMetrics(displayMetrics)
+        height = displayMetrics.heightPixels
+        width = displayMetrics.widthPixels
+
         wallpaperApi.getWallpaperList().enqueue(object : Callback<WallpaperListResponse> {
 
             override fun onFailure(call: Call<WallpaperListResponse>, t: Throwable) {
@@ -47,10 +68,9 @@ class EditFragment : Fragment() {
             }
 
             override fun onResponse(
-                call: Call<WallpaperListResponse>,
-                response: Response<WallpaperListResponse>
+                call: Call<WallpaperListResponse>, response: Response<WallpaperListResponse>
             ) {
-                wallpaperList = response.body()!!.wallpapers
+                wallpaperList = response.body()!!.wallpapers.toList()
                 recyclerAdapter.dataSet = wallpaperList
                 recyclerAdapter.notifyDataSetChanged()
                 Log.d("WallpaperListResponse", response.body()!!.toString())
@@ -63,8 +83,7 @@ class EditFragment : Fragment() {
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_edit, container, false)
@@ -74,18 +93,79 @@ class EditFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         recyclerView = view.findViewById<RecyclerView>(R.id.edit_fragment_recycler_view)
         recyclerAdapter = CustomAdapter(wallpaperList)
+
         recyclerView.adapter = recyclerAdapter
-        recyclerView.layoutManager = GridLayoutManager(requireContext(), 2)
+        recyclerView.layoutManager = GridLayoutManager(requireContext(), 3)
+
+        val tagGroup: TagGroup = view.findViewById(R.id.edit_fragment_tag_layout)
+
+
+
+        tagsResultCallback = object : Callback<TagsResult> {
+            fun errorHandle() {
+                Snackbar.make(
+                    view,
+                    "Error while getting the Tags. Offline functionality will be implemented soon",
+                    Snackbar.LENGTH_LONG
+                ).setAction("Retry") { view ->
+                    wallpaperApi.getTags().enqueue(this)
+                }.show()
+            }
+
+            override fun onResponse(call: Call<TagsResult>, response: Response<TagsResult>) {
+
+                if (response.code() == 200) {
+                    Log.d("tags_response", response.body()!!.tags.toList().toString())
+                    tagGroup.setTags(response.body()!!.tags, emptyArray())
+                } else {
+                    errorHandle()
+                }
+            }
+
+            override fun onFailure(call: Call<TagsResult>, t: Throwable) {
+                errorHandle()
+            }
+        }
+        wallpaperApi.getTags().enqueue(tagsResultCallback)
+        tagGroup.addOnSelectionChangeListener(object : OnTagSelectionChangeListener {
+            override fun selectionChanged(
+                selectedChips: MutableList<Chip>,
+                unselectedChips: MutableList<Chip>
+            ) {
+                val selectedTags =
+                    selectedChips.stream().map { chip -> chip.text }.map { it.toString() }
+                        .collect(Collectors.toList())
+                filterOnTags(selectedTags.toList())
+            }
+        })
     }
 
-    class CustomAdapter(var dataSet: Array<WallpaperInfoObject>) :
+    fun filterOnTags(selectedTags: List<String>) {
+        if (selectedTags.isEmpty()) {
+            shownWallpaper = wallpaperList.toMutableList()
+        } else {
+            shownWallpaper = mutableListOf()
+            for (tag in selectedTags) {
+                for (wallpaper in wallpaperList) {
+                    if (wallpaper.tags.contains(tag) && !shownWallpaper.contains(wallpaper)) {
+                        shownWallpaper.add(wallpaper)
+                    }
+                }
+            }
+        }
+
+        recyclerAdapter.dataSet = shownWallpaper
+        recyclerAdapter.notifyDataSetChanged()
+    }
+
+    inner class CustomAdapter(var dataSet: List<WallpaperInfoObject>) :
         RecyclerView.Adapter<CustomAdapter.ViewHolder>() {
 
         /**
          * Provide a reference to the type of views that you are using
          * (custom ViewHolder)
          */
-        class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
             val imageView: ImageView
 
             init {
@@ -99,22 +179,50 @@ class EditFragment : Fragment() {
             // Create a new view, which defines the UI of the list item
             val view = LayoutInflater.from(viewGroup.context)
                 .inflate(R.layout.wallpaper_thumbnail_item, viewGroup, false)
+            val frame_layout: FrameLayout =
+                view.findViewById<FrameLayout>(R.id.wallpaper_thumbnail_item_layout)
 
+            frame_layout.minimumHeight = (this@EditFragment.width - 20) / 3
             return ViewHolder(view)
         }
 
         // Replace the contents of a view (invoked by the layout manager)
         override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
-            Log.d("recycler_bind", "view binding")
             // Get element from your dataset at this position and replace the
             // contents of the view with that element
-            wallpaperApi.getThumbnail(dataSet[position].name)
+
+            if (selectedWallpaper.contains(viewHolder.adapterPosition)) {
+                viewHolder.imageView.setColorFilter(Color.argb(120, 200, 200, 255));
+            } else {
+                viewHolder.imageView.setColorFilter(Color.argb(0, 200, 200, 255));
+            }
+
+            viewHolder.imageView.setOnLongClickListener {
+                if (!selectedWallpaper.contains(viewHolder.adapterPosition)) {
+                    selectedWallpaper.add(viewHolder.adapterPosition)
+                    viewHolder.imageView.setColorFilter(Color.argb(120, 200, 200, 255));
+                }
+                true
+            }
+
+            viewHolder.imageView.setOnClickListener {
+                if (selectedWallpaper.contains(viewHolder.adapterPosition)) {
+                    selectedWallpaper.remove(viewHolder.adapterPosition)
+                    viewHolder.imageView.setColorFilter(Color.argb(0, 200, 200, 255));
+                } else if (!selectedWallpaper.contains(viewHolder.adapterPosition) && selectedWallpaper.size > 0) {
+                    selectedWallpaper.add(viewHolder.adapterPosition)
+                    viewHolder.imageView.setColorFilter(Color.argb(120, 200, 200, 255));
+                }
+                true
+            }
+
+
+            wallpaperApi.getThumbnail(dataSet[viewHolder.adapterPosition].name)
                 .enqueue(object : Callback<ResponseBody> {
                     override fun onResponse(
-                        call: Call<ResponseBody>,
-                        response: Response<ResponseBody>
+                        call: Call<ResponseBody>, response: Response<ResponseBody>
                     ) {
-                        Log.d("thumbnail_response", dataSet[position].name)
+                        Log.d("thumbnail_response", dataSet[viewHolder.adapterPosition].name)
                         Log.d("thumbnail_response", response.message())
                         val inputStream = response.body()!!.byteStream()
                         val bitmap = BitmapFactory.decodeStream(inputStream)
@@ -123,10 +231,20 @@ class EditFragment : Fragment() {
                     }
 
                     override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
-                        Log.d("thumbnail_response", dataSet[position].name)
+                        Log.d("thumbnail_response", dataSet[viewHolder.adapterPosition].name)
                     }
 
                 })
+
+        }
+
+        override fun onViewRecycled(holder: ViewHolder) {
+            super.onViewRecycled(holder)
+            if (selectedWallpaper.contains(holder.adapterPosition)) {
+                holder.imageView.setColorFilter(Color.argb(120, 200, 200, 255));
+            } else {
+                holder.imageView.setColorFilter(Color.argb(0, 200, 200, 255));
+            }
         }
 
         // Return the size of your dataset (invoked by the layout manager)
