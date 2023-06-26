3381efd48fcf5fa61961ecd5437b1c0de0923435
Lorenz Keefer <clemenzkafer@gmail.com>
2023-06-26 14:42:58 +0200

    send wallpaper to upload works now

--- android_app/app/src/main/java/com/example/wallpaperwizard/MainActivity.kt
+++ android_app/app/src/main/java/com/example/wallpaperwizard/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.wallpaperwizard
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
@@ -9,9 +10,13 @@ import androidx.viewpager2.widget.ViewPager2
 import com.example.wallpaperwizard.Fragments.EditFragment
 import com.example.wallpaperwizard.Fragments.HomeFragment
 import com.example.wallpaperwizard.Fragments.UploadFragment
+import com.example.wallpaperwizard.Fragments.UploadFragmentInterface
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), DataPassInterface {
     private lateinit var viewPager: ViewPager2
+    private var uploadFragment :Fragment = UploadFragment()
+    private var homeFragment :Fragment = HomeFragment()
+    private var editFragment :Fragment = EditFragment()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -22,15 +27,23 @@ class MainActivity : AppCompatActivity() {
         val pagerAdapter = ScreenSlidePagerAdapter(this)
         viewPager.adapter = pagerAdapter
         viewPager.currentItem = 1
-
     }
 
+    override fun passEditWallpaper(wallpaper: MutableList<WallpaperInfoObject>) {
+        viewPager.currentItem=0
+        (uploadFragment as UploadFragmentInterface).set_wallpaper_info_stack(wallpaper)
+        (uploadFragment as UploadFragmentInterface).load_from_wallpaper_info_stack()
+    }
     private inner class ScreenSlidePagerAdapter(fa: FragmentActivity) : FragmentStateAdapter(fa) {
-        val fragmentClassArray = arrayOf(UploadFragment(), HomeFragment(), EditFragment())
+        val fragmentClassArray = arrayOf(uploadFragment, homeFragment, editFragment)
         override fun getItemCount(): Int = fragmentClassArray.size
 
         override fun createFragment(position: Int): Fragment {
             return fragmentClassArray[position]
         }
     }
+}
+
+interface DataPassInterface {
+    fun passEditWallpaper(wallpaper: MutableList<WallpaperInfoObject>)
 }
