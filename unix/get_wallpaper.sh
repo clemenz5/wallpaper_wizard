aebf8d8da366dfcdab1b077665056374436d15db
Lorenz Keefer <clemenzkafer@gmail.com>
2023-07-10 12:32:37 +0200

    fixed minor bugs

--- unix/get_wallpaper.sh
+++ unix/get_wallpaper.sh
@@ -2,14 +2,13 @@
 [ "$1" = "help" ] && echo "See the manpage for actual help" && exit 0
 [ $# -eq 0 ] && echo "You at least have to specify the server url" && exit 1
 cache_dir=$4
-query_url="$1/wallpaper?tags=$2&sync=$3&follow=true"
 retries=10
 wallpaper_path=
 
 if [[ $cache_dir != "" ]]
 then
 	#Get wallpaper name from info object
-	wallpaper_name=$(head --lines 16 /tmp/wallpaper_info | grep name | cut -d " " -f 2 | tr -d '\n' | tr -d '\r') || true
+	wallpaper_name=$(ls -t $cache_dir | head -1) || true
 	wallpaper_path="$cache_dir/$wallpaper_name"
 else
 	wallpaper_path="/tmp/wallpaper.jpg"
@@ -17,6 +16,7 @@ fi
 xwallpaper --zoom "$wallpaper_path"
 
 for i in $(seq 1 $retries); do	
+	query_url="$1/wallpaper?tags=$2&sync=$3&follow=true"
 	if [[ $cache_dir != "" ]]
 	then
 		echo "Query: $query_url&info=true"
@@ -28,6 +28,7 @@ for i in $(seq 1 $retries); do
 		#Check if a wallpaper with the same name already exists
 		if [ -f "$cache_dir/$wallpaper_name" ]
 		then
+			touch "$cache_dir/$wallpaper_name"
     		wallpaper_path="$cache_dir/$wallpaper_name"
 		else
 			query_url="$1/wallpaper/$wallpaper_name"
